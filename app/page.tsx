1046a4eb78a624da3c7b6bf2699638af8e6c5d1e
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-20 08:00:46 +0800

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import LandingPage from "./components/landing";
 
 export default function Home() {
   return (
     <>
       <div className="mx-auto max-w-354">
-        
+        <LandingPage/>
       </div>
     </>      
   );
